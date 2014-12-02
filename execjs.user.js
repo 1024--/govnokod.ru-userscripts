bfa734d024b5edd1288c6341c930f4066c5b271b
1024-- <1024--@users.noreply.github.com>
2014-12-02 12:34:32 +0600

    Добавлен скрипт для выполнения кода.

---
+++ execjs.user.js
@@ -0,0 +1,37 @@
+﻿// ==UserScript==
+// @name Выполнить JavaScript
+// @namespace govnokod
+// @description Добавляет скриптам кнопку "Выполнить"
+// @include http://govnokod.ru/*
+// @include http://www.govnokod.ru/*
+// @version 0.0.1
+// @grant none
+// ==/UserScript==
+
+$('li.hentry:has(a[rel=chapter][text=JavaScript])').each(function(){
+  var entry = $(this),
+      result = $('<pre id="result">...</pre>');
+  
+  var execute = $('<a href="#">Выполнить</a>').click(function(){
+    var code = entry.find('div.entry-content>pre');
+    try {
+      result.text('Успех: ' + String(eval(code.text())));
+    } catch(e) {
+      result.text('Ошибка: ' + e.message);
+    }
+    return false;
+  });
+  
+  var edit = $('<a href="#">Редактировать</a>').click(function(){
+    entry.find('div.entry-content>pre').attr('contentEditable', true);
+    $(this).remove();
+    return false;
+  });
+  
+  var panel = $('<div>')
+    .append(execute)
+    .append(document.createTextNode(' '))
+    .append(edit);
+  
+  entry.find('div.entry-content').after(panel).after(result);
+});
