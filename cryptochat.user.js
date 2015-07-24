8bbc1522bce1882e95beb7430893445cc8b3b20e
1024-- <1024--@users.noreply.github.com>
2015-07-24 16:56:13 +0600

    Добавлен обмен ключами по D-H. Ключ для симметричного шифрования увеличен до 256 бит.

--- cryptochat.user.js
+++ cryptochat.user.js
@@ -3,7 +3,7 @@
 // @namespace govnokod
 // @include http://govnokod.ru/*
 // @include http://www.govnokod.ru/*
-// @version 0.0.4
+// @version 0.0.5
 // @grant none
 // ==/UserScript==
 
@@ -13,7 +13,7 @@
 (function(){
   
   var ID = 'dbdf6b41-e08a-4046-809f-50f513fd0756';
-  
+    
   function SumHandler(n, cb) {
     return function(){
       console.log('SumHandler: ' + (n-1));
@@ -49,7 +49,7 @@
   checksum.LENGTH = 4;
   
   function Key(str) {
-    var m = str.match(/^(?:\(([^)]*)\))?(.{32})$/);
+    var m = str.match(/^(?:\(([^)]*)\))?(.+)$/);
     if(!m) {
       alert('Неправильная строка с ключом: "' + str + '"');
       m = ['','неверный ключ', ''];
@@ -95,7 +95,7 @@
   }
   
   function encrypt(text, key) {
-    var iv_txt = createKey();
+    var iv_txt = createKey(key.key.sigBytes * 8);
     var iv = CryptoJS.enc.Hex.parse(iv_txt);
     
     return 'AES:1:' + 
@@ -103,27 +103,28 @@
       String(CryptoJS.AES.encrypt(checksum(text) + text, key.key, { iv: iv }));
   }
   
-  function createKey(){
-    var arr = new Uint8Array(16);
+  function createKey(N){
+    if(!N) N = 256;
+    N = N / 8 | 0;
+    var arr = new Uint8Array(N);
     crypto.getRandomValues(arr);
     var key = '';
-    for(var i=0; i<16; ++i) {
+    for(var i=0; i<N; ++i) {
       if(arr[i] < 0x10) key += '0';
       key += arr[i].toString(16);
     }
     return key;
   }
   
-  var keys;
-  var currentKey;
+  var keys, currentKey, DHkey, DHprime, Dhgen;
   
   function reloadKeys() {
-    keys = localStorage.getItem(ID + 'keys').split(':').map(function(key){
+    keys = loadKeys().split(':').map(function(key){
       return new Key(key);
     });
 
     currentKey = keys[0];
-    
+        
     updateKeySelector();
   }
   
@@ -133,6 +134,18 @@
 
   function loadKeys() {
     return localStorage.getItem(ID + 'keys') || '';
+  }
+  
+  function reloadDHKey() {
+    DHkey = str2bigInt(loadDHKey(), 16, 2048, 256);
+  }
+  
+  function loadDHKey() {
+    return localStorage.getItem(ID + 'DHkey');
+  }
+  
+  function saveDHKey(key) {
+    localStorage.setItem(ID + 'DHkey', key);
   }
   
   function decryptComments() {
@@ -187,7 +200,7 @@
     var encButton = $('<a href="#">[зашифровать]</a>').click(function(event){
       transformField(function(x){ return encrypt(x, currentKey); });
       event.preventDefault();
-    }).appendTo(container);
+    });
     
     var decButton = $('<a href="#">[расшифровать]</a>').click(function(event){
       transformField(function(x){
@@ -195,7 +208,53 @@
         return decr != null ? decr.text : x;
       });
       event.preventDefault();
-    }).appendTo(container);
+    });
+    
+    var DH1Button = $('<a href="#">[DH:вставить]</a>').click(function(event){
+      var publicKey = powMod(DHgen, DHkey, DHprime);
+      comment.val(comment.val() + 'DHKEY:1:' + bigInt2str(publicKey, 16));
+      event.preventDefault();
+    });
+    
+    var DH2Button = $('<a href="#">[DH:принять]</a>').click(function(event){
+      var s = window.getSelection(), keyString = String(s), publicKey, keyComment = 'DH|new-key';
+      
+      if(!keyString) {
+        publicKey = prompt('Введите публичный ключ того, с кем хотите поговорить.\n' +
+          'Вы также можете выделить текст его комментария с ключом, ' +
+          'чтобы была захвачена строка вида DHKEY:1:PITUX и снова нажать на [DH|принять].');
+        if(publicKey == null) return false;
+        publicKey = publicKey.replace(/^DHKEY:.+?:/, '');
+      } else {
+        var m = keyString.match(/DHKEY:(.+?):([A-Fa-f0-9]+)/);
+        if(!m) {
+          alert('Ключ не найден. Захватите выделением строку вида DHKEY:1:PITUX');
+          return false;
+        }
+        if(m[1] != '1') {
+          alert('Неизвестная версия ' + m[1] + '.');
+          return false;
+        }
+        publicKey = m[2];
+
+        var comment = $(s.anchorNode).closest('div.entry-comment-wrapper');
+        if(comment.length)
+          keyComment = 'DH|' + comment.find('.entry-author>a').text();
+      }
+      
+      var secret = powMod(str2bigInt(publicKey, 16, 2048, 256), DHkey, DHprime);
+      var salt = CryptoJS.lib.WordArray.create();
+      var key = CryptoJS.PBKDF2(bigInt2str(secret, 16), salt, { keySize: 256/32 }).toString();
+      
+      key = prompt('Такой ключ будет добавлен в список ключей для шифрования.\n' +
+      'Вы можете отредактировать ключ или его описание.\n' +
+      'Или нажать Esc для отмены добавления.', '(' + keyComment + ')' + key);
+      if(key == null) return false;
+      saveKeys(loadKeys() + ':' + key);
+      reloadKeys();
+      
+      return false;
+    });
 
     keySelector = $('<select></select>').change(function(){
       var v = Number(keySelector.val());
@@ -213,6 +272,10 @@
       .append('  ')
       .append(decButton)
       .append('  ')
+      .append(DH1Button)
+      .append('  ')
+      .append(DH2Button)
+      .append('  ')
       .append(keySelector)
       .appendTo(info);
   }
@@ -220,21 +283,36 @@
   function init() {
     
     if(!loadKeys()) saveKeys('(automatic)' + createKey());
+    if(!loadDHKey()) saveDHKey(createKey(2048));
     reloadKeys();
+    reloadDHKey();
     appendPanel();
     decryptComments();
     
     $('a.answer, h3>a').live('click', appendPanel);
     $(document).ajaxComplete(appendPanel).ajaxComplete(decryptComments);
     
+    DHgen = int2bigInt(2, 8, 1);
+    DHprime = str2bigInt(
+      'd7efdf8bcb081bb53f0ab2b1621c91e0b181f78f83fc9aafc1f71fa7236311a3' +
+      '065a2ebbc456944ce34c9d2f9fef770a7828c46dd855ff32a1c4feb00e748fe2' +
+      '0ccb4d0b7f0eb0f6d4f27a7bdb904052786829810e8e8101bcf4ec2a76896c37' +
+      '7f5f5751e3675c019ef7e477f682f63596eeb0453ac5cdd09c07528c296362d4' +
+      '9ce3779a6afad9cb2f3d569a32f0a39273f0721ac90cd05933734826147314b6' +
+      '0e8dc792ceef446b0247adf94270264f6f72cc52ce77f16657d07c8c231a0b4e' +
+      '1aeb0756e5caf1930b01b3f019875f57007f39e1f62182c5b081b7f3b1435904' +
+      '2b3b153297afcce5ecb4d8f5fb792b1f7b6f6e314d1375627374f8a8a838d76b',
+      16, 2048, 256
+    );
+
     // (c)пёрто из https://github.com/bormand/govnokod-board/
     var configDialog = $("<li><div><p>Настройки шифрования:</p></div></li>")
       .appendTo($('#userpane > .pane-content > ul'));
     
-    configDialog.append($('<a href="#">Новый ключ</a>')
+    configDialog.append($('<a href="#">Новый ключ AES</a>')
       .click(function(event){
         var key = createKey();
-        var key = prompt('Новый ключ.\n' +
+        key = prompt('Новый ключ для зашифрованной переписки.\n' +
           'Введите свой, если требуется изменить.\n' +
           'Или нажмите Esc для отмены.\n' +
           'Можно оставить комментарий перед ключом в скобках.\n' +
@@ -242,29 +320,43 @@
         if(key == null) return false;
         var ks = loadKeys();
         ks = ks ? key + ':' + ks : key;
-        saveKeys(ks)
+        saveKeys(ks);
         reloadKeys();
         return false;
       }));
       
     configDialog.append('<br/>');
 
-    configDialog.append($('<a href="#">Управление ключами</a>')
+    configDialog.append($('<a href="#">Управление ключами AES</a>')
       .click(function(event){
-        var ks = prompt('Мои ключи.\n' +
+        var ks = prompt('Мои ключи для зашифрованной переписки.\n' +
           'Отредактируйте список (разделены через ":").\n' +
           'Можно оставить комментарий перед ключами в скобках.\n' +
           'Первый ключ станет ключом по умолчанию.\n' +
           'Или нажмите Esc для отмены.', loadKeys());
         if(ks == null) return false;
-        saveKeys(ks);
+        saveKeys(ks || saveKeys('(automatic)' + createKey()));
         reloadKeys();
+        return false;
+      }));
+    
+    configDialog.append('<br/>');
+
+    configDialog.append($('<a href="#">Приватный ключ D-H</a>')
+      .click(function(event){
+        var key = prompt('Мой приватный ключ для обмена ключами.\n' +
+          'Отредактируйте приватный ключ или нажмите Esc для отмены.', loadDHKey());
+        if(key == null) return false;
+        saveDHKey(key);
+        reloadDHKey();
         return false;
       }));
     
   }
   
-  var handler = SumHandler(1, init);
+  var handler = SumHandler(3, init);
   loadScript('http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/aes.js', handler);
+  loadScript('http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/pbkdf2.js', handler);
+  loadScript('http://leemon.com/crypto/BigInt.js', handler);
 
 })();
