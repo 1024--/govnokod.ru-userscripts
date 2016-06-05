0bcef43161a11bc818daaffc3a19fa7fa6505f91
1024-- <1024--@users.noreply.github.com>
2016-06-06 02:11:25 +0600

    Почти добавлена поддержка нового стока; добавлена кнопка разворота поста.

--- infinite_stok.user.js
+++ infinite_stok.user.js
@@ -4,7 +4,7 @@
 // @description Подключает бесконечный сток Борманда к стоку ГК
 // @include http://govnokod.ru/comments
 // @include http://www.govnokod.ru/comments
-// @version 1.1.1
+// @version 2.0.0
 // @grant none
 // ==/UserScript==
 
@@ -29,15 +29,15 @@
     });
     
     return comments.filter(function(comm){
-      return !(comm.thread in stokPosts);
+      return !(comm.thread_id in stokPosts);
     });
   }
 
   function postInfo(entry){
-    var UID = entry.user.id;
-    var PID = entry.thread;
+    var UID = entry.user_id;
+    var PID = entry.thread_id;
     var PID2 = entry.reply_id;
-    var CID = entry.id;
+    var CID = entry.comment_id;
     var postRef = '/' + PID;
     var commName = 'comment' + CID;
     var commRef = '/' + PID + '#comment' + CID;
@@ -45,12 +45,12 @@
       commRef :
       '/comments/' + PID2 + '/post?replyTo=' + CID;
     var userRef = '/user/' + UID;
-    var userName = entry.user.name;
-    var time = entry.published;
-    var hrtime = entry.published;
+    var userName = entry.user_name;
+    var time = entry.posted;
+    var hrtime = entry.posted;
     var avatar = UID == 1 ?
       '/files/avatars/guest_28.png' :
-      'http://www.gravatar.com/avatar/' + entry.user.ava +
+      'http://www.gravatar.com/avatar/' + entry.avatar_hash +
       '?default=http%3A%2F%2Fgovnokod.ru%2Ffiles' +
       '%2Favatars%2Fnoavatar_28.png&amp;r=pg&amp;size=28';
     var commHTML = entry.text;
@@ -73,9 +73,41 @@
                 .append($('<a/>', {href: commRef, name: commName,
                                    title: "Ссылка на комментарий",
                                    'class': "comment-link", text: '#'})))
-              .append($(commHTML))
+              .append($('<li/>', {'class': "entry-comment"})
+                .append($(commHTML)))
               .append($('<a/>', {'class': "answer", href: answerRef,
-                                 text: 'Ответить'}))))));
+                                 text: 'Ответить'}))))))
+      .append($('<div/>', {'class': "show-code"})
+        .append($('<a/>', {href: '/'+PID, 'class': "show-code-trigger", text: 'Показать код ▼'})
+          .click(function(){
+            var $this = $(this);
+            var $holder = $this.parent().children('.code-holder');
+            if($holder.css('display') == 'block') {
+              $this.text('Показать код ▼');
+              $holder.css('display', '');
+            } else {
+              $this.text('Скрыть код ▲');
+              $holder.css('display', 'block');
+              
+              if(!$holder.children().length) {
+                $holder.text('Загружается...');
+                $.ajax({
+                  url: this.href,
+                  success: function(data){
+                    $holder.text('');
+                    $holder.append($(data).find('li.hentry').children());
+                  },
+                  error: function(){
+                    $holder.text('Ошибка загрузки :(');
+                  }
+                });
+              }
+            }
+            return false;
+          }))
+        .append($('<div/>', {'class': "code-holder"}))
+      );
+;
   }
 
   function stokElement(){ return $('ol.posts'); }
@@ -94,17 +126,18 @@
   }
 
   function appendPosts(stok){
-    var REQUIRED_VERSION = 2;
-    if(stok.api_version != REQUIRED_VERSION) {
-      stokElement().append(error('Борманд сменил версию API. Было ' +
-        REQUIRED_VERSION + ', стало ' + stok.api_version + '.' +
-        ' Надеемся на обратную совместимость.'));
-    }
+    // no api_version
+    // var REQUIRED_VERSION = 2;
+    // if(stok.api_version != REQUIRED_VERSION) {
+      // stokElement().append(error('Борманд сменил версию API. Было ' +
+        // REQUIRED_VERSION + ', стало ' + stok.api_version + '.' +
+        // ' Надеемся на обратную совместимость.'));
+    // }
 
-    var comments = filterInfiniteStok(stok.comments);
+    var comments = filterInfiniteStok(stok);
     var fragment = document.createDocumentFragment();
         
-    if(comments.length >= stok.comments.length)
+    if(comments.length >= stok.length)
       fragment.appendChild(warning('Возможно, сток порван.')[0]);
     
     for(var i=0; i < comments.length; ++i)
@@ -122,10 +155,12 @@
     stokElement().append(stub);
     
     $.ajax({
-      url: "http://bormand.tk/gkapi/latest",
+      url: "http://146.185.130.46/ngk/api/v1/latest",
       cache: false,
       success: function(data){
         try {
+          // if(typeof data === 'string')
+            // data = JSON.parse(data);
           appendPosts(data);
         } catch(e){
           appendLoadButton('Ошибка: ' + e.message);
