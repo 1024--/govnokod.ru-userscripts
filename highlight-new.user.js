c089017ed33d6567b06077a6df732952cfeff024
1024-- <1024--@users.noreply.github.com>
2018-03-19 02:57:23 +0700

    Дата треда заменена на номер последнего комментария.

--- highlight-new.user.js
+++ highlight-new.user.js
@@ -3,16 +3,16 @@
 // @description    Подсвечивает новые комментарии
 // @include        http://govnokod.ru/*
 // @include        http://www.govnokod.ru/*
-// @version        1.0.1
+// @version        2.0
 // @grant          none
 // ==/UserScript==
 
 (function() {
 
-function updateState(data, PID, newDate) {
-  // use `date = 0` to enable highlighting comments in unvisited posts
-  // use `date = Date.now() / 1000` to disable highlighting in unvisited posts
-  var out, idx, pid, i = 0, date = 0; // here
+function updateState(data, lastPID, updater) {
+  // use `cid = 0` to enable highlighting comments in unvisited posts
+  // use `cid = Infinity` to disable highlighting in unvisited posts
+  var out, idx, pid, i = 0, cid = 0; // here
   
   while(i < data.length) {
     idx = i;
@@ -22,47 +22,54 @@ function updateState(data, PID, newDate) {
       pid <<= 16;
       pid |= data.charCodeAt(i++);
     }
-    if(pid == PID) {
-      date = data.charCodeAt(i++) << 16 | data.charCodeAt(i++);
+    if(pid == lastPID) {
+      cid = data.charCodeAt(i++) << 16 | data.charCodeAt(i++);
       break;
     } else {
       i += 2;
     }
   }
   
-  data = pid == PID ? data.substring(0, idx) + data.substring(i) : data;
+  data = pid == lastPID ? data.substring(0, idx) + data.substring(i) : data;
   
-  if(isFinite(PID) && PID >= 0 && PID < 0x80000000) {
-    newDate /= 1000;
-    data += (PID >= 0x8000 ? String.fromCharCode(0x8000 | PID >> 16) : '') +
-      String.fromCharCode(PID & 0xffff) + String.fromCharCode(newDate >> 16) +
-      String.fromCharCode(newDate & 0xffff);
+  var lastCID = updater(cid);
+  
+  if(isFinite(lastPID) && lastPID >= 0 && lastPID < 0x80000000) {
+    data += (lastPID >= 0x8000 ? String.fromCharCode(0x8000 | lastPID >> 16) : '') +
+      String.fromCharCode(lastPID & 0xffff) + String.fromCharCode(lastCID >> 16) +
+      String.fromCharCode(lastCID & 0xffff);
   }
   
-  return {
-    data: data,
-    date: date * 1000
-  };
+  return data;
+}
+
+function commentID(comment) {
+  var commentLink = comment.querySelector('a.comment-link');
+  return Number(commentLink.href.match(/comment(\d+)$/)[1]);
+}
+
+function updateComments(prevCID) {
+  var comments = document.querySelectorAll('.entry-comment-wrapper');
+  var lastCID = prevCID;
+
+  for(var i=0; i<comments.length; ++i) {
+    var comment = comments[i];
+    var cid = commentID(comment);
+    if(cid <= prevCID) continue;
+    if(cid > lastCID) lastCID = cid;
+    comment.classList.add('new');
+  }
+  
+  return lastCID;
 }
 
 var post = location.pathname.match(/^\/(\d+)/);
 if(!post) return;
 
-var PARAM = '8a9bd32e-20bc-42c7-bcdd-b65bb1fc2d0b-visited';
+var PARAM = '8a9bd32e-20bc-42c7-bcdd-b65bb1fc2d0b-visited2';
 var postID = +post[1];
-var state = updateState(localStorage.getItem(PARAM) || '', postID, Date.now());
-var lastVisitTime = state.date;
-localStorage.setItem(PARAM, state.data);
-
-var comments = document.querySelectorAll('.entry-comment-wrapper');
-
-for(var i=0; i<comments.length; ++i) {
-  var comment = comments[i];
-  var published = comment.querySelector('abbr.published');
-  if(!published) continue;
-  var p = Number(new Date(published.title)) + 10000; // +10s to catch all up
-  if(!isFinite(p) || p < lastVisitTime) continue;
-  comment.classList.add('new');
-}
+var oldState = localStorage.getItem(PARAM) || '';
+var newState = updateState(oldState, postID, updateComments);
+localStorage.setItem(PARAM, newState);
 
 })();
